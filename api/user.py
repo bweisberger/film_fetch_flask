1840b22fe7330037e5031f33ceac4af17b3c7e79
William Weisberger <bill.weisberger@gmail.com>
2019-08-28 12:29:43 -0600

    commented out save_picture function from register

--- api/user.py
+++ api/user.py
@@ -71,10 +71,10 @@ def login():
 
 @user.route('/register', methods=['POST'])
 def register():
-    img_file = request.files #grab image file
-    payload = request.form.to_dict()
-    img_file_dict = img_file.to_dict()
-    print(img_file_dict, "img_file_dict")
+    # img_file = request.files #grab image file
+    payload = request.to_dict()
+    # img_file_dict = img_file.to_dict()
+    # print(img_file_dict, "img_file_dict")
     print(payload, "payload", type(payload), "type")
 
     payload["email"].lower() #make emails lowercase
@@ -85,17 +85,17 @@ def register():
         #if no user with that email, make one
 
         #hash password using bcrypt
-        payload["password"] = generate_password_hash(payload["password"])
-        file_picture_path = save_picture(img_file_dict["image"]) #save picture path
+        # payload["password"] = generate_password_hash(payload["password"])
+        # file_picture_path = save_picture(img_file_dict["image"]) #save picture path
 
         #add the image property to the form dictionary
-        payload["image"] = file_picture_path
+        # payload["image"] = file_picture_path
 
         #makes the user (1 row in the sql table)
         user = models.Users.create(**payload)
         login_user(user) #from flask_login - sets userid in session
     
-        current_user.image = file_picture_path #current_user comes from login_user(), and we're grabbing the picture path
+        current_user.image = payload['image'] #current_user comes from login_user(), and we're grabbing the picture path
 
         user_dict = model_to_dict(user)
 
